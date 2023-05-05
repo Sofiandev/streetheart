1ec0d8cc52e8d12fb71fecf386d0ef31269faeed
SofianAcDev <sofian.achbabi@gmail.com>
2023-05-05 20:34:52 +0200

    add index

--- index.php
+++ index.php
@@ -1,2 +1,23 @@
 <?php
-require_once 'dbconnect.php';
+
+require_once "model/managers/PostManager.php";
+
+//le but de la page d'accueil étant d'accueillir tous les articles, on récupère tous les articles
+$posts = PostManager::getAllPosts();
+
+
+require_once "./views/partials/header.php";
+require_once "./views/partials/navigation.php";
+?>
+
+<section id="homepage" class="pt-2">
+    <h1 class="text-center mt-5">Bienvenue sur mon blog à propos de mes mangas préférés</h1>
+
+
+    </body>
+
+    </html>
+
+    <?php
+    require_once "./views/partials/footer.php";
+    ?>
